283dab860ddaf39b12880a05005f0be42aec99c3
Hasan MD Atik <akhasannaeem@gmail.com>
2026-01-12 00:27:52 +0600

    feat: mongodb has been connected

--- index.js
+++ index.js
@@ -8,10 +8,46 @@ const port = process.env.PORT || 2000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+
+    
+
+
+
+
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
